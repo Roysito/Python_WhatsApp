f96143c25ec7f44665f07304ad692168d3b44097
Roy Escudero Mendoza <roy_escudero@hotmail.com>
2024-05-16 21:14:20 -0500

    SQLALCHEMY

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+#Configuración de la base de datos SQLITE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///metapython.db'
+app.config['SQLALCHEMY_TRAK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+#Modelo de la tabla LOG
+class log(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    fecha_y_hora = db.Column(db.DateTime, default = datetime.utcnow)
+    texto = db.Column(db.TEXT)
+    
 @app.route('/')
 def hola_mundo():
 
